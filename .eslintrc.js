0a47a80d76c2d555b0bfa783c7a9b716510ff717
hlanaumann@gmail.com <hlanaumann@gmail.com>
2022-06-01 07:11:20 +0200

    Añadida la configuración inicial de prettier con eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: { react: { version: 'detect' } },
+	extends: ['plugin:react/recommended', 'standard', 'eslint-config-prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	extends: ['plugin:react/jsx-runtime'],
+	rules: {},
+}
