4be82a7b8ca545fdfa0f75c67dfc700d982c672d
hlanaumann@gmail.com <hlanaumann@gmail.com>
2022-06-01 11:12:06 +0200

    añadido el componente y sus estilos al button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Layout from './Layout/Layout'
-
+import Button from './components/Button'
+import styles from './components/Button.module.css'
 function App() {
 	return (
 		<Layout>
 			<div className='App'>
 				<h1>Prueba</h1>
 				<p>lorem ipsum es el mejor del mundo</p>
+				<Button props={styles.buttonClassOrange}>Siguiente</Button>
 				<FontAwesomeIcon icon={faCoffee} />
 			</div>
 		</Layout>
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({ children, props }) => {
+	return <button className={props}>{children}</button>
+}
+
+export default Button
